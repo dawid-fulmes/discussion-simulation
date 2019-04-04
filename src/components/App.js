34eb84e50a97fda9cd6aff0529a3f397c545b53c
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-04 16:19:29 +0200

    Add method for get and display random comment
    Method for getting random comments from API has been added. Comments
    are also passed to Discussion.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,44 @@ import './App.css';
 import Discussion from './Discussion';
 
 class App extends Component {
+  state = {
+    commentLog: [],
+  }
+
+
+  //Simulation of other users adding new comments
+  otherUserAddNewComment = () => {
+    const randComment = Math.floor(Math.random() * 500 + 1)
+    const API = "https://jsonplaceholder.typicode.com/comments/" + randComment
+    fetch(API)
+      .then(result => {
+        if (result.status === 200) return result.json()
+        else throw new Error()
+      })
+      .then(newComment => {
+        const randTime = Math.floor(Math.random() * 10000)
+        const commentLog = [...this.state.commentLog]
+        console.log(randTime, commentLog);
+        commentLog.push(newComment)
+        this.setState({ commentLog });
+        setTimeout(this.otherUserAddNewComment, randTime)
+      })
+      .catch(err => console.log(`Something went wrong!
+    ${err}`))
+  }
+
+
+  componentDidMount() {
+    this.otherUserAddNewComment()
+  }
+
   render() {
+    const { commentLog } = this.state
     return (
       <div className="App">
         <main>
           <Article />
-          <Discussion />
+          <Discussion commentLog={commentLog} />
         </main>
       </div>
     );
--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Form from './Form'
 
-const Discussion = () => {
+const Discussion = ({ commentLog }) => {
+    const comments = commentLog.map(comment => (
+        <div>
+            <br />
+            <div>{comment.body}</div>
+            <br />
+        </div>
+    ))
     return (
         <section className="discussion">
             <section>
-                Place for comments
+                {comments}
             </section>
             <Form />
         </section>
