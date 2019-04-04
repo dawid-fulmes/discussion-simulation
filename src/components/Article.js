96a2dd440bd8f474ca3462be511f837d458f2317
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-04 14:22:43 +0200

    Add static content in Article
    Static content has been added in Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import picture from '../img/money.jpg';
 import './Article.css';
 
 
 const Article = () => {
     return (
         <article>
-            It's article!
+            <header>
+                Very controversial article!!!
+            </header>
+            <section>
+                <img src={picture} alt="Lot of money" />
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero qui repellat in inventore natus eius fugiat, a unde deserunt recusandae incidunt vel neque dolorem magni? Vitae dicta quod, expedita consequatur odio quo nobis odit repudiandae velit, quidem laudantium quibusdam fugit delectus in molestiae doloremque quos voluptatum commodi quis error nisi dignissimos dolor aspernatur nesciunt! Hic voluptatem officiis ipsa deserunt cumque expedita quibusdam ipsum commodi temporibus illo facilis error sint modi natus molestiae tempore, non velit perferendis quam cupiditate, ex sed laborum blanditiis at. Quo ut hic, tempora omnis ipsam harum blanditiis vero ad distinctio sint esse aliquid, sapiente quae facilis.</p>
+            </section>
         </article>
     );
 }
