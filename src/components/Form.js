ae98664f2d027e62eef496ee411ec82ab3412b9d
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-10 20:58:27 +0200

    Add a state and handleChange method for Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import './Form.scss'
+
 class Form extends Component {
-    state = {}
+    state = {
+        nickname: '',
+        comment: '',
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target
+        console.log(name, value)
+        this.setState({ [name]: value });
+    }
+
     render() {
+        const { handleChange } = this
+        const { comment, nickname } = this.state
         return (
             <div className="Form">
-                <label>Your nickname: <input type="text" /></label>
-                <textarea placeholder="Your comment..."></textarea>
+                <label>Your nickname: <input name="nickname" value={nickname} onChange={handleChange} type="text" /></label>
+                <textarea name="comment" value={comment} onChange={handleChange} placeholder="your comment..."></textarea>
                 <button>Add comment</button>
             </div>
         );
     }
 }
+
 
 export default Form;
