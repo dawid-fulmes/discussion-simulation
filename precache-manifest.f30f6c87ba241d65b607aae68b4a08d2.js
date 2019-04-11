63dc770f296ef649a97f7bec78661d76a2d11c18
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-04-11 20:42:17 +0200

    Updates

---
+++ precache-manifest.f30f6c87ba241d65b607aae68b4a08d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d805a69e0f99af8d959afb2d2524887",
+    "url": "/discussion-simulation/static/media/money.7d805a69.jpg"
+  },
+  {
+    "revision": "859fdb678105b860635b",
+    "url": "/discussion-simulation/static/js/runtime~main.2dc5d34c.js"
+  },
+  {
+    "revision": "9f5de9a69a785c59d21a",
+    "url": "/discussion-simulation/static/js/main.1ace4383.chunk.js"
+  },
+  {
+    "revision": "8d7e71fc5f551f7cf40d",
+    "url": "/discussion-simulation/static/js/2.b85fd010.chunk.js"
+  },
+  {
+    "revision": "9f5de9a69a785c59d21a",
+    "url": "/discussion-simulation/static/css/main.ab43782f.chunk.css"
+  },
+  {
+    "revision": "4c3bc330580a31f4360fc522147b45e4",
+    "url": "/discussion-simulation/index.html"
+  }
+];
